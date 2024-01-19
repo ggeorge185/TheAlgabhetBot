f012f863afd9b6ee6a15bef77e097ecebc91900d
ggeorge185 <56782229+ggeorge185@users.noreply.github.com>
2024-01-19 11:40:50 +0100

    Updated working MrRobot.java
    The http request file has been added so that the frontend can recll the bacnkend.

--- Backend/src/MrRobot.java
+++ Backend/src/MrRobot.java
@@ -1,49 +1,141 @@
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.synonym.SynonymGraphFilterFactory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.BoostQuery;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class MrRobot {
 
     private static final String INDEX_DIRECTORY = "index";
     private static final String FILES_DIRECTORY = "./AI_Articles";
+    private static int bestDocId = -1;
+    private static final int PORT = 4321;
 
     public static void main(String[] args) {
+    	
         try {
-        	clearIndex();
-            createIndex();
-            search("What is chinese room experiment?");
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            server.createContext("/ask-question", new AskQuestionHandler());
+            server.setExecutor(null);
+            server.start();
+            System.out.println("Server started on port " + PORT);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        
+    	
+    	/*
+        Scanner scanner = new Scanner(System.in);
+        String response = null;
+        String query = null;
+        while(true) {
+            if (args.length < -1) {
+                System.out.println("Please add the query!");
+                response = "Sorry, I don't understand";
+            } else {
+                System.out.print("Ask a question: ");
+                query = scanner.nextLine();
+                query = query.replaceAll("\\byou\\b", "artificial intelligence");
+                try {
+                    clearIndex();
+                    createIndex();
+                    response = search(query);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            System.out.println(response);
+            
+            System.out.print("Was the answer helpful? (yes/no): ");
+            String feedback = scanner.nextLine().toLowerCase();
+
+            // Update document weighting based on user feedback
+            try {
+                updateDocumentWeighting(query, feedback);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        */
+        
+
     }
     
+    static class AskQuestionHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("POST".equals(exchange.getRequestMethod())) {
+                String query = new String(exchange.getRequestBody().readAllBytes());
+                String response = null;
+                query = query.replaceAll("\\byou\\b", "artificial intelligence");
+                try {
+                    clearIndex();
+                    createIndex();
+                    response = search(query);
+                    if(response == null) {
+                    	response = "Sorry, I don't understand :( ";
+                    } 
+                    System.out.println(response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                    
+                    response = "Internal server error";
+                    exchange.sendResponseHeaders(500, response.length());
+                    try (OutputStream os = exchange.getResponseBody()) {
+                        os.write(response.getBytes(StandardCharsets.UTF_8));
+                    }
+                    return;
+                }
+
+                
+                exchange.getResponseHeaders().set("Content-Type", "text/plain");
+                exchange.sendResponseHeaders(200, 0);
+                try (OutputStream os = exchange.getResponseBody()) {
+                    os.write(response.getBytes(StandardCharsets.UTF_8));
+                }
+            }
+        }
+
+    }
+    
+
+    
+
     private static void clearIndex() throws IOException {
         Path indexPath = FileSystems.getDefault().getPath(INDEX_DIRECTORY);
         File indexDir = indexPath.toFile();
 
         if (indexDir.exists()) {
-            
             for (File file : indexDir.listFiles()) {
                 file.delete();
             }
@@ -52,11 +144,19 @@ public class MrRobot {
     }
 
     private static void createIndex() throws IOException {
-        StandardAnalyzer analyzer = new StandardAnalyzer();
+        Analyzer analyzer = CustomAnalyzer.builder()
+                .withTokenizer("WhiteSpace")
+                .addTokenFilter("lowercase")
+                //.addTokenFilter("stop")
+                .addTokenFilter(SynonymGraphFilterFactory.class, "synonyms", "synonyms.txt", "ignoreCase", "true")
+                .addTokenFilter("porterstem")
+                .build();
         Path indexPath = FileSystems.getDefault().getPath(INDEX_DIRECTORY);
         Directory indexDirectory = FSDirectory.open(indexPath);
 
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        // Use BM25Similarity for scoring
+        config.setSimilarity(new BM25Similarity());
         IndexWriter indexWriter = new IndexWriter(indexDirectory, config);
 
         File filesDir = new File(FILES_DIRECTORY);
@@ -64,13 +164,12 @@ public class MrRobot {
 
         if (files != null) {
             for (File file : files) {
-
-                
                 String content = readFileContent(file);
-                
+
                 Document document = new Document();
                 document.add(new TextField("content", content, Field.Store.YES));
                 document.add(new StringField("filename", file.getName(), Field.Store.YES));
+                document.add(new StoredField("content_boost", "1.0")); // Default boost value
                 indexWriter.addDocument(document);
             }
         }
@@ -90,53 +189,98 @@ public class MrRobot {
         return content.toString();
     }
 
-    private static void search(String queryString) throws IOException {
+    private static String search(String queryString) throws IOException {
+        String response = null;
+
         StandardAnalyzer analyzer = new StandardAnalyzer();
         Path indexPath = FileSystems.getDefault().getPath(INDEX_DIRECTORY);
         Directory indexDirectory = FSDirectory.open(indexPath);
 
         IndexSearcher indexSearcher = new IndexSearcher(DirectoryReader.open(indexDirectory));
+        // Use BM25Similarity for scoring during search
+        indexSearcher.setSimilarity(new BM25Similarity());
 
         QueryParser queryParser = new QueryParser("content", analyzer);
 
         try {
-        	Query luceneQuery = preprocessQuery(queryString, queryParser);
-            
-            TopDocs topDocs = indexSearcher.search(luceneQuery, 5);
+            String enhancedQ = enhanceQuery(queryParser.parse(queryString), queryString);
+            Query q = queryParser.parse(enhancedQ);
 
-            System.out.println("Search results for query: " + queryString);
-            boolean lock = false;
-            String response = null;
-            
+            TopDocs topDocs = indexSearcher.search(q, 5);
+
+            System.out.println("Search results for query: " + enhancedQ);
+            float maxScore = Float.NEGATIVE_INFINITY;
+            int bestDocId = -1;
+
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
-                Document document = indexSearcher.doc(scoreDoc.doc);
-                //System.out.println("Filename: " + document.get("filename"));
-                //System.out.println("Content: " + document.get("content"));
-                if(!lock) {
-                	response = document.get("content");
-                	lock = true;
+                if (scoreDoc.score > maxScore) {
+                    bestDocId = scoreDoc.doc;
+                    maxScore = scoreDoc.score;
                 }
             }
             
-            System.out.print("Response: " + response);
+
+            if (bestDocId != -1) {
+                Document bestDocument = indexSearcher.doc(bestDocId);
+                response = bestDocument.get("content").toString();
+                //System.out.println("Best Document - Filename: " + bestDocument.get("filename"));
+                //System.out.println("Best Document - Content: " + bestDocument.get("content"));
+            }
 
         } catch (org.apache.lucene.queryparser.classic.ParseException e) {
             e.printStackTrace();
         }
+
+        return response;
+    }
+
+    private static String enhanceQuery(Query originalQuery, String queryString) {
+        
+        String qTerm = queryString.replaceAll("(?i)^(What|Who) (is|are) ", "").replaceAll("[^a-zA-Z0-9\\s]", "");
+        String whyTerm = queryString.replaceAll("(?i)^(Why)", "").replaceAll("[^a-zA-Z0-9\\s]", "");
+
+        
+        if (queryString.toLowerCase().contains("what is") || (queryString.toLowerCase().contains("who is"))) {
+            return qTerm + " is a";
+
+        } else if (queryString.toLowerCase().contains("what are") || (queryString.toLowerCase().contains("who are"))) {
+            return qTerm + " are";
+
+        } else if (queryString.toLowerCase().startsWith("why")) {
+            return whyTerm + " because";
+
+        }
+        // No enhancement for other queries
+        return queryString;
     }
     
-    private static Query preprocessQuery(String queryString, QueryParser queryParser) throws org.apache.lucene.queryparser.classic.ParseException {
-        if (false || queryString.startsWith("What is") || queryString.startsWith("What are")) {
-            
-            Query definitionBoostQuery = new BoostQuery(queryParser.parse("definition:" + queryString.substring("What is".length())), 2.0f);
-            
-            return new BooleanQuery.Builder()
-                    .add(new BooleanClause(definitionBoostQuery, BooleanClause.Occur.SHOULD))
-                    .add(new BooleanClause(queryParser.parse(queryString), BooleanClause.Occur.SHOULD))
-                    .build();
-        } else {
-            
-            return queryParser.parse(queryString);
+
+    private static void updateDocumentWeighting(String query, String feedback) throws IOException {
+        // Update document weighting based on user feedback for the best document
+        if (bestDocId != -1) {
+            Path indexPath = FileSystems.getDefault().getPath(INDEX_DIRECTORY);
+            Directory indexDirectory = FSDirectory.open(indexPath);
+
+            IndexSearcher indexSearcher = new IndexSearcher(DirectoryReader.open(indexDirectory));
+
+            Document document = indexSearcher.doc(bestDocId);
+
+            // Adjust document weighting based on user feedback
+            float oldBoost = Float.parseFloat(document.get("content_boost"));
+            float newBoost = feedback.equals("yes") ? oldBoost * 1.2f : oldBoost * 0.3f;
+
+            // Update document in the index with the new boost value
+            IndexWriterConfig config = new IndexWriterConfig(new StandardAnalyzer());
+            config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+
+            try (IndexWriter indexWriter = new IndexWriter(indexDirectory, config)) {
+                Document updatedDocument = new Document();
+                updatedDocument.add(new TextField("content", document.get("content"), Field.Store.YES));
+                updatedDocument.add(new StringField("filename", document.get("filename"), Field.Store.YES));
+                updatedDocument.add(new StoredField("content_boost", String.valueOf(newBoost))); // Store the boost value
+
+                indexWriter.updateDocument(new Term("filename", document.get("filename")), updatedDocument);
+            }
         }
     }
 }
