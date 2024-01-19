b8131e56f5f69c99124957a5447ecd047dac7404
ggeorge185 <56782229+ggeorge185@users.noreply.github.com>
2024-01-19 11:35:22 +0100

    Updated app.py
    Final touches of making the Chatbot work

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
+from html import escape
 import os
+import requests  # Install the requests library 
 
 app = Flask(__name__)
 @app.route('/static/<path:filename>')
@@ -15,7 +17,17 @@ def send_message():
     user_message = request.form['user_message']
     bot_response = "This needs to be implemented in java."
 
+    java_backend_url = "http://localhost:4321/ask-question"
+    response = requests.post(java_backend_url, data=user_message)
+
+    bot_response = escape(response.text) if response.ok else "Error communicating with the backend."
+
     return jsonify({'sender': 'bot', 'message': bot_response})
+    
+@app.route('/end_chat')
+def end_chat():
+    return render_template('feedback.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
